611efa8e603c5a217e3f9a68424fb8e7ca3fa66c
Marcin Makowski <marcin.makowski@intel.com>
2016-05-20 15:16:17 +0200

    update licenses information

--- custom_images/postgresql93/scripts/first_run.sh
+++ custom_images/postgresql93/scripts/first_run.sh
@@ -13,6 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Portions Copyright (c) 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 USER=${POSTGRES_USERNAME:-pgadmin}
 PASS=${POSTGRES_PASSWORD:-$(pwgen -s -1 16)}
 DB=${POSTGRES_DBNAME:-}
